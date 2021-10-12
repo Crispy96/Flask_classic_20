ebebdf322ec42707932ceeb61b64162d9a00ed8a
Cristina Rodríguez <crroga96@gmail.com>
2021-10-12 19:54:29 +0200

    Casi acabado

--- balance/forms.py
+++ balance/forms.py
@@ -1,7 +1,14 @@
 #FORMULARIO
 from flask_wtf import FlaskForm
 from wtforms import DateField, StringField, FloatField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
+
+import datetime
+
+def validar_fecha(formulario, campo):
+    hoy = datetime.date.today()  #date de hoy
+    if campo.data > hoy:
+        raise ValidationError("La fecha no puede ser posterior a hoy ")
 
 class MovimientoFormulario(FlaskForm):
     fecha = DateField("Fecha", validators=[DataRequired(message="Debe informar la fecha")])
@@ -12,3 +19,9 @@ class MovimientoFormulario(FlaskForm):
                                             choices=[('G', 'Gasto'), ('I', 'Ingreso')])
 
     submit = SubmitField('Aceptar') 
+
+    #lo defino dentro o lo defino fuera y luego lo adjunto
+    def validate_fecha(self, campo):
+        hoy = datetime.date.today()  #date de hoy
+        if campo.data > hoy:
+            raise ValidationError("La fecha no puede ser posterior a hoy ")
--- balance/views.py
+++ balance/views.py
@@ -1,7 +1,8 @@
 from balance import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, flash
 from balance.models import DBManager
 from balance.forms import MovimientoFormulario
+from date.time import 
 
 ruta_basedatos = app.config.get("RUTA_BASE_DE_DATOS")
 dbManager = DBManager(ruta_basedatos)
@@ -22,12 +23,48 @@ def inicio():
 @app.route("/nuevo", methods=["GET", "POST"])
 def nuevo():
     formulario = MovimientoFormulario()
-    return render_template("nuevo_movimiento.html", el_formulario = formulario)
 
+    if request.method == 'GET':
+        return render_template("nuevo_movimiento.html", el_formulario = formulario)     
+    else:
+        if formulario.validate():
+            consulta = """
+                INSERT INTO movimiento (fecha, concepto, ingreso_gasto, cantidad)
+                 VALUES (:fecha, :concepto, :ingreso_gasto, :cantidad))
+            """
 
+            try:
+                dbManager.modificaSQL(consulta, formulario.data)
+            except Exception as e:
+                print("Se ha producido un error de acceso a base de datos:", e)
+                flash("Se ha producido un error en la base de datos. Consulte con su administrador")
+                return render_template("nuevo_movimiento.html", el_formulario=formulario)
 
-    return "Pagina de alta de movimiento"
+            return redirect(url_for("inicio"))
+        else: 
+            return render_template("nuevo_movimiento.html", el_formulario = formulario)
+        """
+        Validar formulario
+        si la validación es OK -> insertar registro en tabla y redireccionar a /
+        si la validacion es erronea -> devolver el formulario y render_template
+            y preparar nuestra plantilla para gestionar los errores
+        """
 
 @app.route("/borrar/<int:id>", methods=["GET", "POST"])
 def borrar(id):
-    return f"Pagina de borrado de {id}"
+    if request.method == 'GET':
+        consulta = """
+        SELECT id, fecha, concepto, ingreso_gasto, cantidad
+        WHERE id = ?
+        """
+        movimiento = dbManager.consultaSQL(consulta, [id])
+        if len(movimientos)  == 0:
+            flash(f"Movimiento {id} no encontrado")
+            return redirect(url_form("inicio"))
+        el_movimiento= movimientos[0]
+        el_movimiento['fecha']=date.formisoformat(el_movimiento["fecha"])
+        formulario = MovimientoFormulario(data=el_movimiento)
+
+        return render_template("borrar.html", el_formulario=formulario, id=el_movimiento['id'])
+
+    return "holasoyunpost"
